d7c199f2da0dbd7c92662448c6b834509d7fb5fa
liuchuting <liuchuting1@huawei.com>
2024-12-05 14:08:52 +0800

    weight

--- examples/animatediff/scripts/convert_weights.sh
+++ examples/animatediff/scripts/convert_weights.sh
@@ -1,4 +1,5 @@
 wget -c https://download.mindspore.cn/toolkits/mindone/stable_diffusion/sd_v1.5-d0ab7146.ckpt
+python tools/sd_module_convert.py -s sd_v1.5-d0ab7146.ckpt -t models/stable_diffusion/sd_v1.5-d0ab7146.ckpt
 
 # Convert SD dreambooth model
 wget https://civitai.com/api/download/models/78755 -P models/torch_ckpts/ --content-disposition --no-check-certificate
