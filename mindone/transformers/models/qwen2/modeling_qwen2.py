f7a0417d107931edb4f6620d75d75b393310cf26
liuchuting <liuchuting1@huawei.com>
2025-05-27 18:16:28 +0800

    fix

--- mindone/transformers/models/qwen2/modeling_qwen2.py
+++ mindone/transformers/models/qwen2/modeling_qwen2.py
@@ -199,8 +199,8 @@ def apply_rotary_pos_emb(q, k, cos, sin, position_ids, unsqueeze_dim=1):
     Returns:
         `tuple(ms.Tensor)` comprising of the query and key tensors rotated using the Rotary Position Embedding.
     """
-    cos = cos[position_ids].unsqueeze(unsqueeze_dim)
-    sin = sin[position_ids].unsqueeze(unsqueeze_dim)
+    cos = cos.unsqueeze(unsqueeze_dim)
+    sin = sin.unsqueeze(unsqueeze_dim)
     q_embed = (q * cos) + (rotate_half(q) * sin)
     k_embed = (k * cos) + (rotate_half(k) * sin)
     return q_embed, k_embed
